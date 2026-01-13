67d0be395cb5ccd4a71d7da4bcf494a4f98d1084
jose ceballos <a20201556@alumnos.uady.mx>
2026-01-13 03:06:00 -0600

    refactor: mejoramiento del fokin App Runner

--- cocinaruby/src/main/java/com/Service/UserService.java
+++ cocinaruby/src/main/java/com/Service/UserService.java
@@ -7,8 +7,11 @@ import javax.swing.JOptionPane;
 import com.DAO.Daos.UsuarioDAO;
 import com.Model.ModeloUsuario;
 
+import util.session.SessionUsuario;
+
 public class UserService {
     private UsuarioDAO usuarioDAO= new UsuarioDAO();
+    private SessionUsuario instanceSessionUsuario = SessionUsuario.getInstance();
 
     public boolean EsUsuariorRegistrado(ModeloUsuario usuario) {
         //Verificar si esta validacion si pertenece aqui
@@ -27,14 +30,21 @@ public class UserService {
         
         
     }
+    /**
+     * funcion para verificar los campos que introduce el usuario al sistema
+     * @param usuario
+     * @return
+     */
     private boolean ValidarDatosLogin(ModeloUsuario usuario){
     boolean resultado = usuario.getNombreUsuario()!= null && !usuario.getNombreUsuario().isBlank() 
     && usuario.getContrasenaUsuario() != null && !usuario.getContrasenaUsuario().isBlank();
     return resultado;
     }
 
+    //petición a la base de datos para verificar si el ususario es correcto
     private boolean ValidarUsuario(ModeloUsuario usuario) throws SQLException{
     if(usuarioDAO.autenticar(usuario.getNombreUsuario(), usuario.getContrasenaUsuario()) != null){
+        this.instanceSessionUsuario.guardarNombreUsuario(usuario.getNombreUsuario());
         return true;
     }else {
         return false;
