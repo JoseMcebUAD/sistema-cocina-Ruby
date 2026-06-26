bc60c079264b7cc7017348df2a7ca8ddd8303887
jose ceballos <a20201556@alumnos.uady.mx>
2026-06-26 03:42:20 -0600

    add: se agregó el repositorio de RolUsuario

---
+++ src/main/java/com/cocinarubi/dao/RolUsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.cocinarubi.dao;
+
+import com.cocinarubi.entity.RolUsuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolUsuarioRepository extends JpaRepository<RolUsuario, Integer> {
+}
