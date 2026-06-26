e8b5086e7b3f538c989cb66218f5955bb1fcca56
jose ceballos <a20201556@alumnos.uady.mx>
2026-06-26 03:40:11 -0600

    add: se agregaron las capas de DAO, controller y servicio de desayuno

---
+++ src/main/java/com/cocinarubi/dao/DesayunoRepository.java
@@ -0,0 +1,19 @@
+package com.cocinarubi.dao;
+
+import com.cocinarubi.entity.Desayuno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+public interface DesayunoRepository extends JpaRepository<Desayuno, Integer> {
+
+    @Override
+    @Query("SELECT d FROM Desayuno d ORDER BY d.nombreDesayuno ASC")
+    List<Desayuno> findAll();
+
+    boolean existsByNombreDesayuno(String nombreDesayuno);
+
+    @Query("SELECT COUNT(dp) FROM DesayunoPedido dp WHERE dp.desayuno.idDesayuno = :id")
+    long countEnPedidos(@Param("id") int id);
+}
