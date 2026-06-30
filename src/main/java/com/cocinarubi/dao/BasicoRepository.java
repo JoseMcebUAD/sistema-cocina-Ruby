ee51336f278eb86337a5f58a01efbae8ba35266c
jose ceballos <a20201556@alumnos.uady.mx>
2026-06-30 03:19:09 -0600

    fix: hacer el FETCH JOIN con el complemento

--- src/main/java/com/cocinarubi/dao/BasicoRepository.java
+++ src/main/java/com/cocinarubi/dao/BasicoRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
+import java.util.Optional;
 
 public interface BasicoRepository extends JpaRepository<Basico, Integer> {
 
     @Override
-    @Query("SELECT b FROM Basico b JOIN b.comida c ORDER BY c.nombreComida ASC")
+    @Query("SELECT DISTINCT b FROM Basico b JOIN FETCH b.comida c LEFT JOIN FETCH b.complementos bc LEFT JOIN FETCH bc.complemento ORDER BY c.nombreComida ASC")
     List<Basico> findAll();
 
-    @Query("SELECT COUNT(bc) FROM BasicoComplemento bc WHERE bc.basico.idBasico = :id")
-    long countComplementosByBasicoId(@Param("id") int id);
+    @Query("SELECT b FROM Basico b JOIN FETCH b.comida LEFT JOIN FETCH b.complementos bc LEFT JOIN FETCH bc.complemento WHERE b.idBasico = :id")
+    Optional<Basico> findByIdWithComplementos(@Param("id") int id);
 }
