2667a0182357c66ef6bbff3a195785351575c3d6
jose ceballos <a20201556@alumnos.uady.mx>
2026-07-01 04:49:14 -0600

    add: se agregó el método findBySessionToken

--- src/main/java/com/cocinarubi/dao/ClienteRepository.java
+++ src/main/java/com/cocinarubi/dao/ClienteRepository.java
@@ -4,6 +4,7 @@ import com.cocinarubi.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
@@ -14,4 +15,6 @@ public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
     boolean existsBySessionToken(String sessionToken);
 
     boolean existsByUuidCliente(String uuidCliente);
+
+    Optional<Cliente> findBySessionToken(String sessionToken);
 }
