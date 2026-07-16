f98a8dcda20d87628c6151425f784d83fca6302a
jose ceballos <a20201556@alumnos.uady.mx>
2026-07-16 03:06:15 -0600

    fix: la migración de seeder de ruta tenía un , en vez de ; al final

--- src/main/resources/db/migration/V6.1__add_seeder_rutas.sql
+++ src/main/resources/db/migration/V6.1__add_seeder_rutas.sql
@@ -3,4 +3,4 @@ INSERT INTO ruta (nombre, boundary, is_active, tarifa_envio, tiempo_estimado_min
         'Ruta general',
         ST_GeomFromText('POLYGON((-103.3520 20.6680, -103.3390 20.6680, -103.3390 20.6590, -103.3520 20.6590, -103.3520 20.6680))'),
         1, 25.00, 20,6
-    ),
+    );
