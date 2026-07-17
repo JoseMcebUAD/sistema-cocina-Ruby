7ca8fc7666267c452dabff658f1935a219bf20e3
jose ceballos <a20201556@alumnos.uady.mx>
2026-07-16 21:11:10 -0600

    add: se agregó el decorador @enableAsync

--- src/main/java/com/cocinarubi/Application.java
+++ src/main/java/com/cocinarubi/Application.java
@@ -3,8 +3,10 @@ package com.cocinarubi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 //cambiar luego userDetails...
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableAsync
 public class Application {
 
 	public static void main(String[] args) {
