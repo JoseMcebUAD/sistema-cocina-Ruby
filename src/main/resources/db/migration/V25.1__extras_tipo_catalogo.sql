97427ab065e42648d586d4e5a3f81cc0a621cec9
jose ceballos <a20201556@alumnos.uady.mx>
2026-08-05 20:07:05 -0600

    fix: se eliminó el INSERT de EXTRAS en a tipo_entidad en archivo_modulo

--- src/main/resources/db/migration/V25.1__extras_tipo_catalogo.sql
+++ src/main/resources/db/migration/V25.1__extras_tipo_catalogo.sql
@@ -7,13 +7,13 @@
 
 -- 1) Extender el enum en archivo_modulo y archivo
 ALTER TABLE archivo_modulo
-    MODIFY COLUMN tipo_catalogo_producto ENUM('BASICO', 'COMIDA', 'DESAYUNO', 'EXTRAS') NULL;
+    MODIFY COLUMN tipo_catalogo_producto ENUM('BASICO', 'COMIDA', 'DESAYUNO') NULL;
 
 ALTER TABLE archivo
-    MODIFY COLUMN entity_type ENUM('BASICO', 'COMIDA', 'DESAYUNO', 'EXTRAS') NULL;
+    MODIFY COLUMN entity_type ENUM('BASICO', 'COMIDA', 'DESAYUNO') NULL;
 
 -- 2) Sembrar el módulo EXTRAS
 INSERT INTO archivo_modulo (nombre_modulo, tipo_catalogo_producto, id_categoria, ruta, archivos_aceptados)
-VALUES ('EXTRAS', 'EXTRAS', NULL,
+VALUES ('EXTRAS', NULL, 5,
         'cocina_rubi/extras',
         JSON_ARRAY('image/jpeg', 'image/png', 'image/webp'));
